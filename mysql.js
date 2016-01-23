541d87f616a28194c7d028ec57819971db1ae737
h4r5h4 <harsha.ms7@gmail.com>
2016-01-23 01:43:40 -0800

    Date Time Update

--- mysql.js
+++ mysql.js
@@ -22,7 +22,34 @@ con.end(function(err) {
   // before sending a COM_QUIT packet to the MySQL server.
 });*/
 
-var pack = { twitch_id: 1234, name: 'Artour', viewer:count:256, position:10, game:"Dota 2" };
+   Date.prototype.yyyymmdd = function() {
+   var yyyy = this.getFullYear().toString();
+   var mm = (this.getMonth()+1).toString();
+   var dd  = this.getDate().toString();
+   return yyyy + "-" + (mm[1]?mm:"0"+mm[0]) + "-" + (dd[1]?dd:"0"+dd[0]);
+  };
+
+   Date.prototype.yyyymmdd = function() {
+   var yyyy = this.getFullYear().toString();
+   var mm = (this.getMonth()+1).toString();
+   var dd  = this.getDate().toString();
+   return yyyy + "-" + (mm>10?mm:"0"+mm) + "-" + (dd>10?dd:"0"+dd);
+  };
+ Date.prototype.hhmmss = function() {
+   var hh = this.getHours().toString();
+   var mm = this.getMinutes().toString();
+   var ss  = this.getSeconds().toString();
+   return (hh>10?hh:"0"+hh) + "-" + (mm>10?mm:"0"+mm) + "-" + (ss>10?ss:"0"+ss);
+  };
+
+d = new Date();
+var n = d.yyyymmdd();
+var t = d.hhmmss();
+
+
+
+
+var pack = {date:n, time:t , twitch_id: 1234, name: 'Artour', viewer:count:256, position:10, game:"Dota 2" };
 con.query('INSERT INTO TopChannels SET ?', pack, function(err,res){
   if(err) throw err;
 
