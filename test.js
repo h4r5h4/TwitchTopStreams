42f8646cf1f2f77930cabbc38d80a34541a1fe6b
EC2 Default User <ec2-user@ip-172-31-19-1.ec2.internal>
2016-01-24 00:38:07 +0000

    Working Update

--- test.js
+++ test.js
@@ -2,6 +2,33 @@ var app = require('express')();
 var server = require('http').createServer(app).listen(process.env.PORT || 80);
 var io = require('socket.io').listen(server);
 var request = require('request');
+var mysql = require("mysql");
+var char = '';
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "rockerz",
+  database: "vulcun"
+});
+
+
+
+Date.prototype.yyyymmdd = function() {
+var yyyy = this.getFullYear().toString();
+var mm = (this.getMonth()+1).toString();
+var dd  = this.getDate().toString();
+return yyyy + "-" + (mm>10?mm:"0"+mm) + "-" + (dd>10?dd:"0"+dd);
+};
+Date.prototype.hhmmss = function() {
+var hh = this.getHours().toString();
+var mm = this.getMinutes().toString();
+var ss  = this.getSeconds().toString();
+return (hh>10?hh:"0"+hh) + ":" + (mm>10?mm:"0"+mm) + ":" + (ss>10?ss:"0"+ss);
+};
+
+
+
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -10,29 +37,33 @@ app.get('/', function(req, res) {
 setInterval(function() {
 
 request('https://api.twitch.tv/kraken/streams?limit=15', function (error, response, body) {
-    //Check for error
     if(error){
         return console.log('Error:', error);
     }
-
-    //Check for right status code
     if(response.statusCode !== 200){
         return console.log('Invalid Status Code Returned:', response.statusCode);
     }
-
-    //All is good. Print the body
+    d = new Date();
+    var n = d.yyyymmdd();
+    var t = d.hhmmss();
     var parsedBody = JSON.parse(body)
-    var i,char = '';
-    for(i in parsedBody.streams){
-    char += parsedBody.streams[i].channel.display_name + "," + parsedBody.streams[i].viewers + "," + parsedBody.streams[i].game + "<br>";
-   }  
+    char = '';
+    for(var i=0;i<15;i++){
+      var pos = parseInt(i+1)
+      console.log(pos);
+      var pack = {date:n, time:t, viewer_count:parsedBody.streams[i].viewers, name: parsedBody.streams[i].channel.name,game:parsedBody.streams[i].game, position:pos};
+      con.query('INSERT INTO TopChannels SET ?', pack, function(err,res){
+        if(err) throw err;
+      
+        console.log('Last insert ID:', res.insertId);
+      });
+    char += parsedBody.streams[i].channel.name + "," + parsedBody.streams[i].viewers + "," + parsedBody.streams[i].game + "<br>";
+   }
 io.emit('update', char);
-//console.log(parsedBody.streams[i].channel.display_name + "," + parsedBody.streams[i].viewers + "," + parsedBody.streams[i].game); }// Show the HTML for the Modulus homepage.
-
 });
-   
+}, 60000);
 
-       
-    
-}, 5000);
-
+io.on('connection', function(socket){
+  socket.emit('new conn', char);
+  //console.log(char);
+});
