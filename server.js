b3d8e0a0b1209d4bda6260481bd3d16e5585083f
EC2 Default User <ec2-user@ip-172-31-19-1.ec2.internal>
2016-01-24 04:31:55 +0000

    Final Update

--- server.js
+++ server.js
@@ -5,66 +5,83 @@ var request = require('request');
 var mysql = require("mysql");
 var finalBody = '';
 
+//DB Credentials
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "rockerz",
-  database: "vulcun"
+    host: "localhost",
+    user: "root",
+    password: "rockerz",
+    database: "vulcun"
 });
 
 
-
+//Function to get Current Date in MySQL Date format
 Date.prototype.yyyymmdd = function() {
-var yyyy = this.getFullYear().toString();
-var mm = (this.getMonth()+1).toString();
-var dd  = this.getDate().toString();
-return yyyy + "-" + (mm>10?mm:"0"+mm) + "-" + (dd>10?dd:"0"+dd);
+    var yyyy = this.getFullYear().toString();
+    var mm = (this.getMonth() + 1).toString();
+    var dd = this.getDate().toString();
+    return yyyy + "-" + (mm > 10 ? mm : "0" + mm) + "-" + (dd > 10 ? dd : "0" + dd);
 };
+
+//Function to get Current Time in MySQL Time format
 Date.prototype.hhmmss = function() {
-var hh = this.getHours().toString();
-var mm = this.getMinutes().toString();
-var ss  = this.getSeconds().toString();
-return (hh>10?hh:"0"+hh) + ":" + (mm>10?mm:"0"+mm) + ":" + (ss>10?ss:"0"+ss);
+    var hh = this.getHours().toString();
+    var mm = this.getMinutes().toString();
+    var ss = this.getSeconds().toString();
+    return (hh > 10 ? hh : "0" + hh) + ":" + (mm > 10 ? mm : "0" + mm) + ":" + (ss > 10 ? ss : "0" + ss);
 };
-
-
 
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/client.html');
 });
 
+//Sending updates every 1 minute
 setInterval(function() {
 
-request('https://api.twitch.tv/kraken/streams?limit=15', function (error, response, body) {
-    if(error){
-        return console.log('Error:', error);
-    }
-    if(response.statusCode !== 200){
-        return console.log('Invalid Status Code Returned:', response.statusCode);
-    }
-    d = new Date();
-    var n = d.yyyymmdd();
-    var t = d.hhmmss();
-    var parsedBody = JSON.parse(body)
-    finalBody = '';
-    for(var i=0;i<15;i++){
-      var pos = parseInt(i+1)
-      var vCount = parsedBody.streams[i].viewers;
-      var cName = parsedBody.streams[i].channel.name;
-      var gName = parsedBody.streams[i].game;
-      var pack = {date:n, time:t, viewer_count: vCount, name:cName,game:gName, position:pos};
-      con.query('INSERT INTO TopChannels SET ?', pack, function(err,res){
-        if(err) throw err;
-      
-        //console.log('Last insert ID:', res.insertId);
-      });
-    finalBody += cName + " - " + vCount + " - " + gName + "<br>";
-   }
-io.emit('update', finalBody);
-});
-}, 5000);
+//Fetch Data from Twitch API
+    request('https://api.twitch.tv/kraken/streams?limit=15', function(error, response, body) {
+        if (error) {
+            return console.log('Error:', error);
+        }
+        if (response.statusCode !== 200) {
+            return console.log('Invalid Status Code Returned:', response.statusCode);
+        }
+        d = new Date();
+        var n = d.yyyymmdd();
+        var t = d.hhmmss();
+        var parsedBody = JSON.parse(body)
+        finalBody = '';
 
-io.on('connection', function(socket){
-  socket.emit('new conn', finalBody);
+        //Iterating through the JSON Object
+        for (var i = 0; i < 15; i++) {
+            var pos = parseInt(i + 1)
+            var vCount = parsedBody.streams[i].viewers;
+            var cName = parsedBody.streams[i].channel.name;
+            var gName = parsedBody.streams[i].game;
+            var pack = {
+                date: n,
+                time: t,
+                viewer_count: vCount,
+                name: cName,
+                game: gName,
+                position: pos
+            };
+
+            //Inserting fetched data into MySQL database
+            con.query('INSERT INTO TopChannels SET ?', pack, function(err, res) {
+                if (err) throw err;
+
+                //console.log('Last insert ID:', res.insertId);
+            });
+            finalBody += cName + " - " + vCount + " - " + gName + "<br>";
+        }
+
+        //Sending data to all connected clients
+        io.emit('update', finalBody);
+    });
+}, 60000);
+
+//Sending data whenever a cient is connected
+io.on('connection', function(socket) {
+    socket.emit('new conn', finalBody);
 });
